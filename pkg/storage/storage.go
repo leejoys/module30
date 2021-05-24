569d512b857b81118b134436fa9a81cc641349d4
leejoys <44557202+leejoys@users.noreply.github.com>
2021-05-24 09:02:45 +0300

    methods done

--- pkg/storage/storage.go
+++ pkg/storage/storage.go
@@ -15,60 +15,65 @@ type Storage struct {
 	db *pgxpool.Pool
 }
 type Task struct {
-	id          int
-	opened      int64
-	closed      int64
-	author_id   int
-	assigned_id int
-	title       string
-	content     string
+	Id         int
+	Opened     int64
+	Closed     int64
+	AuthorId   int
+	AssignedId int
+	Title      string
+	Content    string
 }
 
 func (s *Storage) NewTask(t Task) (int, error) {
 	var id int
-	err:=s.db.QueryRow(`INSERT INTO tasks VALUES`,t).Scan(&id)
+	err := s.db.QueryRow(context.Background(),
+		`INSERT INTO tasks(title,content) 
+		VALUES $1,$2 RETURNING id;`, t.Title, t.Content).Scan(&id)
 
-if err != nil {
-	return 0, err
-}
+	if err != nil {
+		return 0, err
+	}
 
 	return id, nil
 }
 
 func (s *Storage) Tasks(id, authorId int) ([]Task, error) {
-	
-	rows,err:=s.db.Query(context.Background(),
-	`SELECT 
-	id,
-	opened,
-	closed,
-	author_id,
-	assigned_id,
-	title,
-	context
-	FROM TABLE tasks 
-	WHERE ($1=0 OR id=$1) AND ($2=0 OR author_id=$2)
-	ORDER BY id`,id,authorId)
-	
+
+	rows, err := s.db.Query(context.Background(),
+		`SELECT 
+		id,
+		opened,
+		closed,
+		author_id,
+		assigned_id,
+		title,
+		content
+		FROM tasks 
+		WHERE ($1=0 OR id=$1) AND ($2=0 OR author_id=$2)
+		ORDER BY id;`, id, authorId)
+
 	if err != nil {
 		return nil, err
 	}
 
 	var tasks []Task
-for range rows{
-	err:=rows.Next()
-	if err != nil {
-		return nil, err
+	for rows.Next() {
+		var t Task
+		err := rows.Scan(
+			&t.Id,
+			&t.Opened,
+			&t.Closed,
+			&t.AuthorId,
+			&t.AssignedId,
+			&t.Title,
+			&t.Content,
+		)
+
+		if err != nil {
+			return nil, err
+		}
+		tasks = append(tasks, t)
 	}
-
-	t,err:=
-
-	if err != nil {
-		return nil, err
-	}
-
-
-}
 
 	return tasks, rows.Err()
 }
