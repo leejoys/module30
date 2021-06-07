96316a9b234158ec8a6206b10d61fe0ee97eac12
leejoys <44557202+leejoys@users.noreply.github.com>
2021-06-07 17:45:27 +0300

    methods ready

--- pkg/storage/storage.go
+++ pkg/storage/storage.go
@@ -1,5 +1,13 @@
 package storage
 
+//[ ]ТЗ:
+//[ ]Получение списка задач,
+//[ ]Получения информации о конкретной задаче по ее номеру,
+//[ ]Создание задачи,
+//[ ]Обновление задачи,
+//[ ]Удаление задачи,
+//[ ]Создание массива задач.
+
 import (
 	"context"
 
@@ -38,7 +46,7 @@ func New(connstr string) (*Storage, error) {
 	return s, nil
 }
 
-// NewTask создаёт новую задачу и возвращает её id.
+// [ ]NewTask создаёт новую задачу и возвращает её id.
 func (s *Storage) NewTask(t Task) (int, error) {
 	var id int
 	err := s.db.QueryRow(context.Background(),
@@ -52,7 +60,7 @@ func (s *Storage) NewTask(t Task) (int, error) {
 	return id, nil
 }
 
-// Tasks возвращает список задач из БД.
+// [ ]Tasks возвращает список задач из БД.
 func (s *Storage) Tasks(id, authorId int) ([]Task, error) {
 
 	rows, err := s.db.Query(context.Background(),
@@ -92,4 +100,71 @@ func (s *Storage) Tasks(id, authorId int) ([]Task, error) {
 	}
 
 	return tasks, rows.Err()
+}
+
+//[ ]Получения информации о конкретной задаче по ее номеру,
+func (s *Storage) Task(id int) (Task, error) {
+	var t Task
+	err := s.db.QueryRow(context.Background(),
+		`SELECT 
+		id,
+		opened,
+		closed,
+		author_id,
+		assigned_id,
+		title,
+		content
+		FROM tasks 
+		WHERE id=$1;`,
+		id).Scan(
+		&t.Id,
+		&t.Opened,
+		&t.Closed,
+		&t.AuthorId,
+		&t.AssignedId,
+		&t.Title,
+		&t.Content,
+	)
+	if err != nil {
+		return Task{}, err
+	}
+
+	return t, nil
+}
+
+//[ ]Обновление задачи,
+func (s *Storage) Update(t Task) error {
+	_, err := s.db.Exec(context.Background(), `
+	UPDATE tasks
+	SET title=$2, content=$3
+	WHERE id=$1;`, t.Id, t.Title, t.Content)
+	return err
+}
+
+//[ ]Удаление задачи,
+func (s *Storage) Delete(id int) error {
+	_, err := s.db.Exec(context.Background(), `
+	DELETE FROM tasks 
+	WHERE id=$1;`, id)
+	return err
+}
+
+//[ ]Создание массива задач.
+func (s *Storage) NewTasks(tasks []Task) error {
+	tx, err := s.db.Begin(context.Background())
+	if err != nil {
+		return err
+	}
+	for _, t := range tasks {
+		_, err = tx.Exec(context.Background(), `
+	INSERT INTO	
+	tasks(title, content)
+	VALUES ($1,$2);`, t.Title, t.Content)
+		if err != nil {
+			tx.Rollback(context.Background())
+			return err
+		}
+	}
+	tx.Commit(context.Background())
+	return nil
 }
